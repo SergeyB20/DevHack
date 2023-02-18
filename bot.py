81a4638d94a49e0896f214b362ea74d13628706f
SergeyB20 <125741718+SergeyB20@users.noreply.github.com>
2023-02-18 16:43:14 +0300

    раздел меню

--- bot.py
+++ bot.py
@@ -5,8 +5,19 @@ bot = telebot.TeleBot('6148105017:AAEqPdBYRg3z5K9RLq9AUbeRd1Y1e4L3nno')
 @bot.message_handler(commands=['start']) #стартовая команда
 def start(message):
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    help = types.KeyboardButton('Помощь')
-    markup.add(help)
-    bot.send_message(message.chat.id, '👋Привет! Я телеграмм-бот🤖, который проверяет ссылки и QR-коды на безопасность, что бы начать со мной рабоать напиши в чат «меню»❤️', reply_markup=markup)
+    Menu = types.KeyboardButton('Меню')
+    markup.add(Menu)
+    bot.send_message(message.chat.id, '👋Привет! Я телеграмм-бот🤖, который проверяет ссылки и QR-коды на безопасность, что бы начать со мной работать нажми на кнопку «меню»❤️', reply_markup=markup)
 
+
+@bot.message_handler(content_types=['text'])
+def get_text_messages(message): 
+    if message.text == 'Меню': 
+        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        
+        Check = types.KeyboardButton('Проверить безопасность сайта')
+        Help = types.KeyboardButton('Помощь')
+        markup.add(Check, Help)   
+        bot.send_message(message.from_user.id, "Выберите команду для продолжения работы", reply_markup=markup)  
+        
 bot.polling()
