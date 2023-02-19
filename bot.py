7f79e3b303f70ae05563a437d545b9caefbdd15f
SergeyB20 <125741718+SergeyB20@users.noreply.github.com>
2023-02-19 13:24:41 +0300

    полностью готовый бот

--- bot.py
+++ bot.py
@@ -1,65 +1,95 @@
-import telebot
-from telebot import types
-import  ssl,requests, urllib.request
-from pyzbar.pyzbar import decode
-import cv2
-from io import BytesIO
+from aiogram import types, executor, Dispatcher, Bot
+from aiogram.dispatcher import FSMContext
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.types import ReplyKeyboardMarkup
+from data_base import good_url, bad_url, data
+import requests
+from fake_useragent import UserAgent
 from PIL import Image
+from pyzbar.pyzbar import decode
+from PIL import Image
+from Forms import form
 
 
 
-bot = telebot.TeleBot('6148105017:AAEqPdBYRg3z5K9RLq9AUbeRd1Y1e4L3nno')
-@bot.message_handler(commands=['start']) 
-def start(message):
-    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    bot.send_message(message.chat.id, '👋Привет! Я телеграмм-бот🤖, который проверяет ссылки и QR-коды на безопасность, что бы начать со мной работать просто напиши свою ссылку❤️', reply_markup=markup)
+TOKEN = '6148105017:AAEqPdBYRg3z5K9RLq9AUbeRd1Y1e4L3nno'
+bot = Bot(token=TOKEN)
+dp = Dispatcher(bot, storage=MemoryStorage())
+keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
+rating = 5
+abra = {}
 
-@bot.message_handler(content_types=["photo"])
-def decrypting(message):  
-	
- 
 
-    
-            
+@dp.message_handler(commands=['start'])
+async def get_text_messages(message: types.Message, state: FSMContext):
+    btn1 = types.KeyboardButton(text='проверка ссылки', resize_keyboard=True)
+    btn2 = types.KeyboardButton(text='проверка QR', resize_keyboard=True)
+    keyboard.add(btn1, btn2)
+    await bot.send_message(message.chat.id, f'👋Привет, {message.from_user.username}! Я телеграмм-бот🤖, который проверяет ссылки и QR-коды на безопасность, что бы начать со мной рабоать отправь мне ссылку или QR-код', reply_markup=keyboard)
+    await form.type.set()
 
-@bot.message_handler(content_types=['text'])
-def check(message):
-    url = message.text
-    try:
-        response = requests.get(url)
+@dp.message_handler(state= form.type)
+async def meny1(message: types.Message, state: FSMContext):
+  text = message.text
+  if text == 'проверка QR':
+    await bot.send_message(message.chat.id, f'Пришли мне изображение QR-кода')
+    await form.qr.set()
+  if text == 'проверка ссылки':
+    await bot.send_message(message.chat.id, f'отправь мне ссылку на проверку')
+    await form.href.set()
+
+@dp.message_handler(content_types=["photo"], state= form.qr)
+async def download_photo(message: types.Message):
+    await message.photo[-1].download('img/am.jpg')
+    decocdeQR = decode(Image.open('img/am.jpg'))
+    a = (decocdeQR[0].data.decode('ascii'))
+    await bot.send_message(message.chat.id, f'Вот ссылка в вашего QR кода {a}, для дальнейшей работы скопируйте и вставьте эту ссылку')
+    await form.type.set()   
+                      
+@dp.message_handler(state= form.href)
+async def meny_fun(message: types.Message, state: FSMContext):
+  url = message.text
+  btn1 = types.KeyboardButton('меню')
+  q = ReplyKeyboardMarkup().add(btn1)
+  abra['url'] = url
+  print(abra['url'])
+  try:
+        response = requests.get(abra['url'], headers={'User-Agent': UserAgent().chrome})
         if response.status_code == 200:
             rating = 5
+            a = '✅HTTPS доступно'
         else:
             rating = rating - 1
-            c = 1
-    except requests.exceptions.RequestException:
-        bot.send_message(message.chat.id, f'неверный URL, попробуйте ввести еще раз')
-        
-    try:
-        r = requests.get(url)
-        if (str(r.history)) == ('[<Response [302]>]'):
-            bot.send_message(message.chat.id, 'Запрос был перенаправлен')
+            a = '❌HTTPS Недоступно'
+        try:
+            r = requests.get(abra['url'], verify=True)
+            ssl_cert = '✅Ссылка имеет SSL сертификат'
+        except requests.exceptions.SSLError:
+            rating -= 1
+            ssl_cert = '❌Ссылка не имеет SSL сертификата'
+  except requests.exceptions.RequestException:
+        await bot.send_message(message.chat.id, f'неверный URL, попробуйте ввести еще раз')
+        await form.href.set()
+  finally:
+        r = requests.get(abra['url'], headers={'User-Agent': UserAgent().chrome})
+        if r.history:
             rating = rating - 1
+            rs =  '❌Запрос был перенаправлен'
+            abra['lastpos'] = r.status_code, r.url
         else:
-            
-            bot.send_message(message.chat.id, 'Запрос не  был перенаправлен')
-            
+            rs =  '✅Запрос не был перенаправлен'
         if rating == 2:
-           
+            btn1 = types.KeyboardButton(text='проверка ссылки',resize_keyboard=True)
+            btn2 = types.KeyboardButton(text='проверка QR',resize_keyboard=True)
+            q = ReplyKeyboardMarkup(resize_keyboard=True).add(btn1,btn2)
+            await bot.send_message(message.chat.id, f'Скорее всего эта ссылка не безопасна.\nХарактеристика:\n{a}\n{rs}\n Рейтинг: {rating}/5⭐️', reply_markup=q)
             
-            bot.send_message(message.chat.id, f'К сожалению ссылка не прошла проверку на безопасность :(\n Рейтинг: {rating}/5⭐️')
-            if a == 1:
-             bot.send_message(message.chat.id, 'Отсутствует SSL Сертификат')
-            if b == 1:
-                bot.send_message(message.chat.id, 'Ваш запрос был перенаправлен')
-            if c == 1:
-                bot.send_message(message.chat.id, 'Отсутствует поддержка HTTPS')
+            await form.type.set()
         if rating > 2:
+            btn1 = types.KeyboardButton(text='проверка ссылки',resize_keyboard=True)
+            btn2 = types.KeyboardButton(text='проверка QR',resize_keyboard=True)
+            q = ReplyKeyboardMarkup(resize_keyboard=True).add(btn1,btn2)
+            await bot.send_message(message.chat.id, f'Отлично! Ссылка оказалась безопасной.\nХарактеристика:\n{ssl_cert}\n{a}\n{rs}\n Рейтинг: {rating}/5⭐️ ', reply_markup=q)
             
-            
-            bot.send_message(message.chat.id, f'Ура! Ссылка прошла проверку и оказалась безопасной, теперь можно спокойно переходить по ней!\n Ваша ссылка прошла проверок - {rating}/5⭐️ ')
-    except:
-        print(1)
-            
-                
-bot.polling()
+            await form.type.set()
+executor.start_polling(dp)
